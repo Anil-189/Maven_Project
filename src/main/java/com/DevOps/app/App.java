d8078b4c544113e6d94a9e097ae3313cc4aed221
Anil-189 <anilgowda.r18@gmail.com>
2021-05-29 11:42:33 +0530

    Changed some lines to run Jenkins git poll

--- src/main/java/com/DevOps/app/App.java
+++ src/main/java/com/DevOps/app/App.java
@@ -1,4 +1,4 @@
-package com.DevOps.app;
+(package com.DevOps.app;
 
 /**
  * Hello world!
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	System.out.println( "Hello this is Anil" );
     }
 }
